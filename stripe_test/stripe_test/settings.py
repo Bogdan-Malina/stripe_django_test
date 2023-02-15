de42fd8efb68db6362d95485812ff022b5230940
HABO3HNK <melinbodb@yandex.ru>
2023-02-15 20:49:01 +0300

    Edit docker

--- stripe_test/stripe_test/settings.py
+++ stripe_test/stripe_test/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['51.250.84.205']
 
 # Application definition
 
